13787a22a8e87d4a8abb6daca232568d6097c41f
darrenliu <darrenl2025@student.cis.edu.hk>
2023-03-07 12:48:25 +0800

    Added Person and Person Factory class

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,32 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setFirstName(String newFirstName){
+        this.firstName = newFirstName;
+    }
+    public void setLastName(String newLastName){
+        this.lastName = newLastName;
+    }
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+    public String toString(){
+        return (firstName + "" + lastName + ", " + age);
+    }
+
 }
+
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -9,14 +9,31 @@ public class PersonFactory {
     }
 
     public void addPerson(Person person) {
-
+        int i;
+        for(i = 0; i <= allPersons.size(); i = i + 1){
+            if(person.getLastName().compareTo((allPersons.get(i)).getLastName()) < 1){
+                allPersons.add(i,person);
+            }
+        }
     }
 
     public ArrayList<Person> under18() {
-
+        ArrayList<Person> under18 = new ArrayList<>();
+        for(int i = 0; i <= allPersons.size(); i = i + 1){
+            if(allPersons.get(i).getAge() < 18){
+                under18.add(allPersons.get(i));
+            }
+        }
+        return under18;
     }
 
     public boolean checkLastName(String lastName) {
-
+        boolean yes = false;
+        for(int i = 0; i <= allPersons.size(); i = i + 1){
+            if (lastName.compareTo(allPersons.get(i).getLastName()) == 0){
+                yes = true;
+            }
+        }
+        return yes;
     }
 }
